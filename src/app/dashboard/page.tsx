27334baa100ee61f435470196987dd6ee01e1927
Ellis Collinson <elliscollinson@me.com>
2023-09-30 12:10:32 +0100

    dashboard header installed, pretest

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,48 @@
 import { Outfit } from "next/font/google";
 import Image from "next/image";
+import styles from "./dashboard.css";
 
 const outfit = Outfit({
-    subsets: ["latin-ext"],
-})
+  subsets: ["latin-ext"],
+});
 
 export default function Page() {
   return (
     <>
-      <main className="flex min-h-screen">
+      <main className="flex">
         {/* title hero */}
-        <div className="bg-yellow-400 h-3/4 absolute top-0 left-0 right-0 flex justify-center items-center px-64">
+        <div
+          className=" md:h-full 2xl:h-2/3 absolute top-0 left-0 right-0 flex justify-center items-center px-36"
+          style={{
+            clipPath: "polygon(100% 0, 0 0, 0 100%, 100% calc(100% - 15vw))",
+            backgroundImage: "url('/static/img/yellow_bg.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
           {/* left of landing hero */}
-          <div className="bg-red-500 w-full">
-            <h1 className={`${outfit.className} text-7xl`}>Generic Portfolio Title</h1>
-            <h2 className="font-sans text-6xl">Something catchy</h2>
-          </div>
+          <div className="max-w-screen-xl flex items-center">
+            <div className="w-full">
+              <h1 className={`${outfit.className} text-8xl text-neutral-800`}>
+                Ellis Collinson
+              </h1>
+              <h2
+                className={`${outfit.className} font-sans text-5xl text-neutral-700`}
+              >
+                My snazzy portfolio
+              </h2>
+            </div>
+
           {/* right side of landing hero */}
-          <div className="bg-blue-500 w-full flex justify-center items-center">
-            <Image src="/static/img/cathedral.png" alt="cathedral" width="500" height="500" />
+          <div className="w-full ">
+            <Image
+              src="/static/img/cathedral.png"
+              alt="cathedral"
+              width="500"
+              height="500"
+            />
           </div>
+        </div>
         </div>
         <div className="flex h-full items-center justify-center"></div>
       </main>
