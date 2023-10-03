2b3add4d0d6e4e1b6c41804809c7b5375cf8a19d
Ellis Collinson <elliscollinson@me.com>
2023-10-03 21:08:41 +0100

    ✨feat: adding two new sections.

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,51 +1,17 @@
+import { LuMenu } from "react-icons/lu";
+
 export default function Header() {
-    return (
-        <nav className="flex justify-center absolute z-50 w-screen flex items-center  flex-wrap bg-gradient-to-r  p-6">
-           <div className="w-screen max-w-screen-xl">
-            <div className="flex items-center flex-shrink-0 text-black mr-6">
-                <span className="font-semibold text-xl tracking-tight">Ellis Collinson</span>
-            </div>
-            <div className="block lg:hidden">
-                <button
-                    className="flex items-center px-3 py-2 border rounded text-black border-black hover:text-white hover:border-white"
-                >
-                    <svg
-                        className="fill-current h-3 w-3"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <title>Menu</title>
-                        <path
-                            d="M0 0h20v20H0V0zm2 2v16h16V2H2zm3 5h10v2H5V7zm0 4h10v2H5v-2zm0 4h10v2H5v-2z"
-                        />
-                    </svg>
-                </button>
-            </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div className="text-sm lg:flex-grow">
-                </div>
-                <div>
-                    <a
-                        href="#responsive-header"
-                        className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
-                    >
-                        Docs
-                    </a>
-                    <a
-                        href="#responsive-header"
-                        className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
-                    >
-                        Examples
-                    </a>
-                    <a
-                        href="#responsive-header"
-                        className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white"
-                    >
-                        Blog
-                    </a>
-                </div>
-            </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="flex justify-center absolute z-50 w-screen flex items-center flex-wrap bg-gradient-to-r py-6 px-20">
+      <div className="w-screen max-w-screen-xl flex justify-between">
+        <span className="font-semibold text-xl min-w-max text-black">
+          Ellis Collinson
+        </span>
+
+        <div className="block lg:flex lg:items-center lg:w-auto">
+          <LuMenu className="text-2xl"/>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <main className="h-screen">
         <Header />
         {/* title hero */}
-        <div
+        <section
           className="md:h-full 2xl:h-3/5 flex justify-center "
           style={{
             // gives the yellow background a slanted edge
@@ -23,15 +23,15 @@ export default function Page() {
           }}
         >
           {/* left of landing hero */}
-          <div className="flex flex-row items-center flex-col lg:flex-row w-full max-w-screen-xl">
+          <div className="flex flex-row items-center flex-col lg:flex-row w-full max-w-screen-xl xl:p-0 p-20">
             <div className="w-full">
               <h1
-                className={`${outfit.className} text-5xl sm:text-6xl md:text-8xl text-neutral-800`}
+                className={`${outfit.className} text-5xl lg:text-7xl text-neutral-800`}
               >
                 another portfolio site
               </h1>
               <h2
-                className={`${outfit.className} font-sans text-5xl lg:5xl text-neutral-800`}
+                className={`${outfit.className} font-sans text-3xl lg:text-4xl text-neutral-800`}
               >
                 made by ellis collinson
               </h2>
@@ -39,8 +39,7 @@ export default function Page() {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum
                 asperiores atque quae autem fugiat doloremque? Quisquam
-                voluptas, voluptatum, quibusdam, quos voluptate dolorum
-                asperiores atque quae autem fugiat doloremque?
+                voluptas, voluptatum, quibusdam.
               </p>
             </div>
 
@@ -54,17 +53,58 @@ export default function Page() {
               />
             </div>
           </div>
-        </div>
+        </section>
         <section className="flex justify-center">
           <div className="max-w-screen-xl w-full text-center p-52 ">
-            <h3 className={`${outfit.className} text-6xl font-medium`} >web design and development is my thing.</h3>
+            <h3 className={`${outfit.className} text-6xl font-medium`}>
+              web design and development is my thing.
+            </h3>
             <p className="font-sans mt-5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptas, voluptatum, quibusdam, quos voluptate dolorum
-              asperiores atque quae autem fugiat doloremque? Quisquam voluptas,
-              voluptatum, quibusdam, quos voluptate dolorum asperiores atque
-              quae autem fugiat doloremque?
+              voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores
+              atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum,
+              quibusdam, quos voluptate dolorum asperiores atque quae autem
+              fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos
+              voluptate dolorum asperiores atque quae autem fugiat doloremque?
             </p>
+          </div>
+        </section>
+        <section
+          className={`md:h-full 2xl:h-3/5 flex justify-center items-center text-center flex flex-col `}
+          style={{
+            // clipPath: "polygon(0 0, 100% 10vw, 100% 100%, 0 100%)",
+            backgroundColor: "#EDEDED",
+          }}
+        >
+          <h1 className={`text-5xl font-normal ${outfit.className}`}>Some fluff here</h1>
+          <div className="flex flex-row items-center flex-col lg:flex-row w-full max-w-screen-xl xl:p-0 px-20 grid-cols-3 grid gap-8">
+            <div className="bg-red-400">
+              <img src="/static/img/emo.png" alt="cathedral" />
+              <h3>Title</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
+                ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum
+                dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+            <div className="bg-red-400">
+              <img src="/static/img/emo.png" alt="cathedral" />
+              <h3>Title</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
+                ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum
+                dolor sit amet consectetur adipisicing elit.
+              </p>{" "}
+            </div>
+            <div className="bg-red-400">
+              <img src="/static/img/emo.png" alt="cathedral" />
+              <h3>Title</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
+                ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum
+                dolor sit amet consectetur adipisicing elit.
+              </p>{" "}
+            </div>
           </div>
         </section>
       </main>
