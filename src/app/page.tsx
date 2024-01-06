108a6c2304f0d6df2069d5d53c112fa4f4cd61ed
Ellis Collinson <elliscollinson@me.com>
2024-01-06 10:37:23 +0000

    Format CSS and inline styles for further development.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,12 @@ const outfit = Outfit({
 
 export default function Page() {
   return (
-    <>
+    <main>
       {/* background holder */}
       <section
-        className="flex justify-center h-[900px] sm:h-[900px] md:h-[900px] md:h-[1000px] xl:h-[750px] 2xl:h-[1000px] w-full"
+        className="
+        flex 
+        justify-center"
         style={{
           // gives the yellow background a slanted edge
           clipPath: "polygon(100% 0, 0 0, 0 100%, 100% calc(100% - 20vw))",
@@ -23,51 +25,84 @@ export default function Page() {
         }}
       >
         {/* container */}
-        <div className="container mx-5 md:mx-10 h-full flex flex-col xl:flex-row items-center">
+        <div
+          className="
+        container
+       mx-5 
+       md:mx-10 
+       h-full flex 
+       flex-col 
+       xl:flex-row 
+       items-center"
+        >
           {/* <div className={`bg-gray-500 absolute container h-12`}>Header</div> */}
+          {/* left side, adjusting the padding on behalf of the right side */}
           <div
-            className={`${outfit.className} p-10 flex flex-col xl:w-2/3 lg:pr-20`}
+            className={`
+          ${outfit.className} 
+          flex 
+          flex-col 
+          px-10 
+          py-20 
+          lg:pr-2 
+          xl:py-80
+          2xl:py-96
+          xl:w-2/3 
+          `}
           >
             <h1>Hey there, Im Ellis Collinson</h1>
             <h2>A portfolio built with love (and Next.js).</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores
-              atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum,
-              quibusdam, quos voluptate dolorum asperiores atque quae autem
-              fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos
-              voluptate dolorum asperiores atque quae autem fugiat doloremque?
-            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores atque quae autem fugiat doloremque?</p>
           </div>
-            <Image
-              src="/static/img/ellis.png"
-              alt="cathedral"
-              width="450"
-              height="450"
-              className="h-full w-auto lg:h-auto "
-            />
+
+          {/* right side */}
+          <Image
+            src="/static/img/ellis.png"
+            alt="A picture of Ellis Collinson in grey scale"
+            width="450" // defining the width and height that next.js will use to optimise the image
+            height="450"
+            className="   
+           h-full
+           w-auto
+           "
+          />
         </div>
       </section>
 
-      {/* second */}
+      {/* about me */}
       <section className="flex justify-center">
         <div
-          className={`${outfit.className} container flex justify-center h-96 items-center p-10 sm:p-20 md:p-40 lg:p-96 flex-col text-center`}
+          className={`
+        ${outfit.className} 
+        container
+        flex
+        flex-col
+        justify-center
+        items-center
+        text-center
+        py-32
+        lg:py-64
+        lg:px-10
+        xl:py-64
+        m-7
+        xl:-mt-32
+       `}
         >
-          <h3 className="pb-10">I’m a web designer and developer.</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores
-            atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum,
-            quibusdam, quos voluptate dolorum asperiores atque quae autem fugiat
-            doloremque? Quisquam voluptas, voluptatum, quibusdam, quos voluptate
-            dolorum asperiores atque quae autem fugiat doloremque?
-          </p>
+          <h3 className="pb-8 lg:pb-10">I’m a web designer and developer.</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores atque quae autem fugiat doloremque?</p>
         </div>
       </section>
 
       <section
-        className={`${outfit.className} w-full h-screen flex justify-center items-center flex-col p-72`}
+        className={`
+        ${outfit.className} 
+        w-full 
+        h-screen 
+        flex 
+        justify-center 
+        items-center 
+        flex-col 
+        p-72`}
         style={{
           // gives the yellow background a slanted edge
           // clipPath: "polygon(100% calc(25vw), 0 0, 0 100%, 100% 100%)",
@@ -91,108 +126,6 @@ export default function Page() {
           </div>
         </div>
       </section>
-
-      <main className="h-screen">
-        <Header />
-        {/* title hero */}
-        <section
-          className="md:h-full 2xl:h-3/5 flex justify-center "
-          style={{
-            // gives the yellow background a slanted edge
-            clipPath: "polygon(100% 0, 0 0, 0 100%, 100% calc(100% - 10vw))",
-            backgroundImage: "url('/static/img/yellow_bg.png')",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          {/* left of landing hero */}
-          <div className="flex flex-row items-center flex-col lg:flex-row w-full max-w-screen-xl xl:p-0 sm:p-20 p-10">
-            <div className="w-full">
-              <h1
-                className={`${outfit.className} text-5xl lg:text-7xl text-neutral-800`}
-              >
-                another portfolio site
-              </h1>
-              <h2
-                className={`${outfit.className} font-sans text-3xl lg:text-4xl text-neutral-800`}
-              >
-                made by ellis collinson
-              </h2>
-              <p className="font-sans mt-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam voluptas, voluptatum, quibusdam, quos voluptate dolorum
-                asperiores atque quae autem fugiat doloremque? Quisquam
-                voluptas, voluptatum, quibusdam.
-              </p>
-            </div>
-
-            {/* right side of landing hero */}
-            <div className="w-full flex justify-center">
-              <Image
-                src="/static/img/cathedral.png"
-                alt="cathedral"
-                width="500"
-                height="500"
-              />
-            </div>
-          </div>
-        </section>
-        <section className="flex justify-center">
-          <div className="max-w-screen-xl w-full text-center p-52 ">
-            <h3 className={`${outfit.className} text-6xl font-medium`}>
-              web design and development is my thing.
-            </h3>
-            <p className="font-sans mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores
-              atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum,
-              quibusdam, quos voluptate dolorum asperiores atque quae autem
-              fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos
-              voluptate dolorum asperiores atque quae autem fugiat doloremque?
-            </p>
-          </div>
-        </section>
-        <section
-          className={`md:h-full 2xl:h-3/5 flex justify-center items-center text-center flex flex-col `}
-          style={{
-            // clipPath: "polygon(0 0, 100% 10vw, 100% 100%, 0 100%)",
-            backgroundColor: "#EDEDED",
-          }}
-        >
-          <h1 className={`text-5xl font-normal ${outfit.className}`}>
-            Some fluff here
-          </h1>
-          <div className="flex flex-row items-center flex-col lg:flex-row w-full max-w-screen-xl xl:p-0 px-20 grid-cols-3 grid gap-8">
-            <div className="bg-red-400">
-              <img src="/static/img/emo.png" alt="cathedral" />
-              <h3>Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
-                ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum
-                dolor sit amet consectetur adipisicing elit.
-              </p>
-            </div>
-            <div className="bg-red-400">
-              <img src="/static/img/emo.png" alt="cathedral" />
-              <h3>Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
-                ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum
-                dolor sit amet consectetur adipisicing elit.
-              </p>{" "}
-            </div>
-            <div className="bg-red-400">
-              <img src="/static/img/emo.png" alt="cathedral" />
-              <h3>Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
-                ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum
-                dolor sit amet consectetur adipisicing elit.
-              </p>{" "}
-            </div>
-          </div>
-        </section>
-      </main>
-    </>
+    </main>
   );
 }
