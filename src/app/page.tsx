aff4074c9ee3f347a553f4d839017ef0714bf306
Ellis Collinson <elliscollinson@me.com>
2024-01-29 16:35:04 +0000

    Update global styles and typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,17 +37,15 @@ export default function Page() {
           }}
         >
           {/* header hero container */}
-          <div className="custom-structure h-screen min-h-[1100px] flex-col-reverse justify-center gap-3 lg:min-h-[1200px] xl:min-h-[700px] xl:flex-row xl:justify-between xl:gap-28">
+          <div className="custom-structure h-screen min-h-[1200px] flex-col-reverse justify-center gap-3 lg:min-h-[1200px] xl:min-h-[800px] xl:flex-row xl:justify-between xl:gap-20">
             {/* left side */}
             <div
               className={`${outfit.className} text-center xl:w-7/12 xl:text-left`}
             >
               <span className="text-3xl font-medium">Hi there,</span>
               <h1>{`I'm Ellis Collinson.`}</h1>
-              <span className="font-sans text-lg font-normal">
-                {`
-                  I'm a web designer and developer with a passion for creating beautiful, functional, and impactful online experiences. I'm skilled in both the artistic and technical aspects of web development, allowing me to bridge the gap between design vision and seamless execution.
-                `}
+              <span className="text-lg font-light xl:text-xl">
+                {`I'm a web designer and developer with a passion for creating beautiful, functional, and impactful online experiences. I'm skilled in both the artistic and technical aspects of web development, allowing me to bridge the gap between design vision and seamless execution.`}
                 <br />
                 <br />
                 {`
@@ -72,16 +70,28 @@ export default function Page() {
         {/* about me */}
         <section>
           <div
-            className={` ${outfit.className} custom-structure m-7 flex-col py-28 text-center lg:px-10 lg:py-48 xl:-mt-32 xl:px-56 xl:py-72`}
+            className={`${outfit.className} custom-structure m-7 flex-col py-28 text-center lg:px-10 lg:py-48 xl:-mt-32 xl:px-56 xl:py-72`}
           >
-            <h3 className="pb-8 lg:pb-10">I’m a web designer and developer.</h3>
+            <h3 className="pb-8 lg:pb-10">
+              Welcome to My Creative Playground.
+            </h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptas, voluptatum, quibusdam, quos voluptate dolorum asperiores
-              atque quae autem fugiat doloremque? Quisquam voluptas, voluptatum,
-              quibusdam, quos voluptate dolorum asperiores atque quae autem
-              fugiat doloremque? Quisquam voluptas, voluptatum, quibusdam, quos
-              voluptate dolorum asperiores atque quae autem fugiat doloremque?
+              This website serves as a comprehensive repository for tracking my
+              personal goals, ongoing projects, and notable achievements. Beyond
+              being a personal resource, it also functions as a platform to
+              exhibit my skills and competencies to potential employers and
+              clients. I envision this website as an invaluable tool for
+              self-reflection, allowing me to assess my growth and evolution as
+              both a designer and developer. <br />
+              <br />
+              Constructed using Next.js, a React framework, with styling
+              implemented through Tailwind CSS, and design conceptualised using
+              Figma, this website reflects a commitment to utilising
+              cutting-edge technologies. Hosted on Vercel, the platform ensures
+              seamless accessibility and performance. In the immediate future,
+              the site is poised to evolve into a small-scale Content Management
+              System (CMS), facilitating the hosting of a blog to share
+              insights, progress updates, and creative ideas.{" "}
             </p>
           </div>
         </section>
@@ -97,47 +107,59 @@ export default function Page() {
         `}
         >
           <div
-            className={` m-7 flex flex-col items-center py-14 text-center lg:w-8/12 lg:py-56
+            className={` custom-structure flex flex-col items-center py-28 text-center lg:py-48 xl:py-72
         `}
           >
-            <h3 className="text-4xl">Im also pretty good at other stuff.</h3>
-            <div className="grid grid-cols-1 justify-items-center gap-10 pt-10 md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-4">
+            <h3 className="text-6xl">Im also pretty good at other stuff.</h3>
+            <div className="grid grid-cols-1 justify-items-center gap-20 pt-10 md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-4">
               <div className="flex flex-col gap-4 ">
-                <img src="/static/img/emo.png" alt="" />
+                <img
+                  className="p-10"
+                  src="/static/img/graphic-design.png"
+                  alt=""
+                />
                 <h4>Graphic Design</h4>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptas, voluptatum, quibusdam, quos voluptate
-                  dolorum asperiores atque quae autem fugiat doloremque?
+                  I love creating small-scale graphics, such as logos and icons.
+                  Many of these designs have been uploaded and have seen{" "}
+                  <b>14,703*</b> downloads and counting .
                 </p>
               </div>
 
               <div className="flex flex-col gap-4 ">
-                <img src="/static/img/emo.png" alt="" />
+                <img className="p-10" src="/static/img/blueprint.png" alt="" />
                 <h4>System Architecture</h4>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptas, voluptatum, quibusdam, quos voluptate
-                  dolorum asperiores atque quae autem fugiat doloremque?
+                  Scoping, designing, and implementing systems is a forte of
+                  mine. I have experience with both monolithic and microservice
+                  architectures.
                 </p>
               </div>
 
               <div className="flex flex-col gap-4 ">
-                <img src="/static/img/emo.png" alt="" />
+                <img
+                  className="p-10"
+                  src="/static/img/cloud-computing.png"
+                  alt=""
+                />
                 <h4>Cloud Computing</h4>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptas, voluptatum, quibusdam, quos voluptate
-                  dolorum asperiores atque quae autem fugiat doloremque?
+                  I have experience constructing and deploying my own cloud
+                  solutions using AWS, Azure, and GCP. I also have experience
+                  with Docker and Kubernetes.
                 </p>
               </div>
               <div className="flex flex-col gap-4 ">
-                <img src="/static/img/emo.png" alt="" />
+                <img
+                  className="p-10"
+                  src="/static/img/app-development.png"
+                  alt=""
+                />
                 <h4>App Development</h4>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptas, voluptatum, quibusdam, quos voluptate
-                  dolorum asperiores atque quae autem fugiat doloremque?
+                  Thanks to React Native, I have built a series of mobile apps
+                  all with different purposes and audiences. I have also
+                  experimented with Flutter.
                 </p>
               </div>
             </div>
@@ -145,23 +167,101 @@ export default function Page() {
         </section>
 
         <section className="flex flex-col ">
-          <h3 className="pb-5 pt-10 text-center text-4xl">
+          <h3 className="${outfit.className} pb-5 pt-10 text-center text-4xl ">
             My Personal Projects
           </h3>
-          <div className="flex flex-col lg:flex-row">
-            <div className="w-full bg-yellow-500 ">
-              <div className="h-96 bg-green-500 lg:h-1/2">Top</div>
-              <div className="h-96 bg-green-800 lg:h-1/2">Bottom</div>
+          <div className="m-3 grid h-screen min-h-[1200px] grid-cols-2 gap-2">
+            <div
+              style={{
+                backgroundImage: "url('/static/img/Improved_spotify.png')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div
+                className="flex h-full w-full flex-col justify-end p-10"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              >
+                <h5>Improved Spotify (Design)</h5>
+                <p className="text-white">
+                  Nostrud non in eiusmod qui incididunt labore enim enim. Dolor
+                  irure eiusmod et eiusmod Lorem minim exercitation occaecat
+                  aliquip eiusmod adipisicing. Sint dolor ipsum aliqua culpa
+                  proident occaecat voluptate ut consequat id pariatur
+                  consectetur esse. Dolore sint minim magna nostrud qui pariatur
+                  adipisicing quis laboris aliqua. Laborum nostrud cupidatat
+                  veniam aliqua ad.
+                </p>
+              </div>
             </div>
             <div
-              className="
-            h-96
-              w-full
-              bg-blue-500
-              lg:h-[75vh]
-      "
+              style={{
+                backgroundImage: "url('/static/img/cms.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
             >
-              Right
+              <div
+                className="flex h-full w-full flex-col justify-end p-10"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              >
+                <h5>Personal CMS</h5>
+                <p className="text-white">
+                  Nostrud non in eiusmod qui incididunt labore enim enim. Dolor
+                  irure eiusmod et eiusmod Lorem minim exercitation occaecat
+                  aliquip eiusmod adipisicing. Sint dolor ipsum aliqua culpa
+                  proident occaecat voluptate ut consequat id pariatur
+                  consectetur esse. Dolore sint minim magna nostrud qui pariatur
+                  adipisicing quis laboris aliqua. Laborum nostrud cupidatat
+                  veniam aliqua ad.
+                </p>
+              </div>
+            </div>
+            <div
+              style={{
+                backgroundImage: "url('/static/img/react.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div
+                className="flex h-full w-full flex-col justify-end p-10"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              >
+                <h5>React Showcase</h5>
+                <p className="text-white">
+                  Nostrud non in eiusmod qui incididunt labore enim enim. Dolor
+                  irure eiusmod et eiusmod Lorem minim exercitation occaecat
+                  aliquip eiusmod adipisicing. Sint dolor ipsum aliqua culpa
+                  proident occaecat voluptate ut consequat id pariatur
+                  consectetur esse. Dolore sint minim magna nostrud qui pariatur
+                  adipisicing quis laboris aliqua. Laborum nostrud cupidatat
+                  veniam aliqua ad.
+                </p>
+              </div>
+            </div>
+            <div
+              style={{
+                backgroundImage: "url('/static/img/cms.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div
+                className="flex h-full w-full flex-col justify-end p-10"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              >
+                <h5>Mini Social Network</h5>
+                <p className="text-white">
+                  Nostrud non in eiusmod qui incididunt labore enim enim. Dolor
+                  irure eiusmod et eiusmod Lorem minim exercitation occaecat
+                  aliquip eiusmod adipisicing. Sint dolor ipsum aliqua culpa
+                  proident occaecat voluptate ut consequat id pariatur
+                  consectetur esse. Dolore sint minim magna nostrud qui pariatur
+                  adipisicing quis laboris aliqua. Laborum nostrud cupidatat
+                  veniam aliqua ad.
+                </p>
+              </div>
             </div>
           </div>
         </section>
